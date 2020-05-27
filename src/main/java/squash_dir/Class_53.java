a5a5d35b981d22c078790d178796397a4cd41f5e
viktoria.bozhko <viktoria.bozhko@gmail.com>
2020-05-27 12:51:31 +0300

    Class_52 committed

---
+++ src/main/java/squash_dir/Class_53.java
@@ -0,0 +1,6 @@
+package squash_dir;
+
+public class Class_53 {
+    public void foo53() {
+    }
+}
---
+++ src/main/java/squash_dir/Class_55.java
@@ -0,0 +1,6 @@
+package squash_dir;
+
+public class Class_55 {
+    public void foo55() {
+    }
+}
